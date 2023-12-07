d9d8ec8746a53f0f159a25e74b5ca78ec7bc96ba
jimccc0207 <150311630+jimccc0207@users.noreply.github.com>
2023-12-07 11:57:12 +0800

    Inherited properties

--- 1130/student.py
+++ 1130/student.py
@@ -1,4 +1,4 @@
-class student:
+class Student:
     schoolName ="南台科技大學"
     schooladdress = "南台街1號"
     
@@ -9,46 +9,77 @@ class student:
         self.id = id
         self.credit = credit
         self.gpa = gpa
-    def getschoolName(self):
+
+    @property
+    def student_name(self):
+        print(f'"{self.name}" was accessed')
+        return self.name
+
+    @student_name.setter
+    def student_name(self,value):
+       print(f'"{self.name}" is now "{value}".')
+       self.name = value
+
+    @student_name.deleter
+    def student_name(self):
+        print(f'"{self.name}" was deleted')
+        del self.name
+
+if __name__ == "__main__":
+        student1 =Student("John Doe", 20, "S12345", 3.5)
+        print(student1.student_name)
+        student1.student_name = "Petrer Pan"
+        del student1.student_name
+        print(student1.student_name)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def getschoolName(self):
         return self.schoolName
-    def setschoolName(self, value):
+def setschoolName(self, value):
         self.schoolName = value
 
-    def getdepartment(self):
+def getdepartment(self):
         return self.department
-    def setdepartment(self, value):
+def setdepartment(self, value):
         self.department = value
     
-    def getdirector(self):
+def getdirector(self):
         return self.director
-    def setdirector(self, value):
+def setdirector(self, value):
         self.director = value
     
-    def getname(self):
+def getname(self):
         return self.name
-    def setname(self, value):
+def setname(self, value):
         self.name = value
     
-    def getid(self):
-        return self.id
-    def setid(self, value):
+def getid(self):
+    return self.id
+def setid(self, value):
         self.id = value
     
-    def getaddress(self):
+def getaddress(self):
         return self.credit
-    def setschooladdress(self, value):
+def setschooladdress(self, value):
         self.schooladdress = value
     
-    def getcredit(self):
+def getcredit(self):
         return self.credit
-    def setcredit(self, value):
-        self.credit = value
+def setcredit(self, value):
+       self.credit = value
     
-    def getgpa(self):
+def getgpa(self):
         return self.gpa
-    def setschool(self, value):
+def setschool(self, value):
         self.school = value
     
-st1 = student("CSIE","洪國均", "張晉嘉","4B0G0139", 60, 4.00)#建立類別為student的物件st1
+st1 = Student("CSIE","洪國均", "張晉嘉","4B0G0139", 60, 4.00)#建立類別為student的物件st1
 print(st1.getschoolName())#呼叫副函式getschoolName()
 print(st1.getid())#呼叫副函式getid()
---
+++ 1207/python_inheritance.py
@@ -0,0 +1,58 @@
+class Sports:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def sports_name(self):
+        return self._name
+
+    @sports_name.setter
+    def sports_name(self, value):
+        self._name = value
+    
+    def practice(self):
+        print("Doing Sports practice")
+
+class LandSports(Sports):
+    def __init__(self, name, field):
+        super().__init__(name)
+        self._field = field
+    
+    @property
+    def landsports_field(self):
+        return self._field
+    
+    @landsports_field.setter
+    def landsports_field(self, value):
+        self._field = value
+    def practice(self):
+        print("Doing Land Sports practice")
+
+class WaterSports(Sports):
+    def __init__(self, name, activity):
+        super().__init__(name)
+        self._activity = activity
+    
+    @property
+    def watersports_activity(self):
+        return self._activity
+    
+    @watersports_activity.setter
+    def watersports_activity(self, value):
+        self._activity = value
+    def practice(self):
+        print("Doing Water Sports practice")
+
+
+if __name__ == "__main__":
+    baseball = LandSports("baseball", "baseball field")
+    print(baseball.sports_name)
+    print(baseball.landsports_field)
+
+    water_skiing = WaterSports("Water Skiing", "Strao on your skis and fly across the water")
+    print(water_skiing.sports_name)
+    print(water_skiing.watersports_activity)
+
+    sports = Sports("Softball")
+    print(sports.sports_name)
+    print(sports.practice())
